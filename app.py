75d001fa1d0e14e3a0681fe8d97ff6f1b3e594f6
Abner moreno <abnerck9@gmail.com>
2026-03-06 00:36:22 +0000

    Actualización desde PythonAnywhere

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-# app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__)
+
+# Middleware para forzar HTTPS y www
+@app.before_request
+def before_request():
+    if request.url.startswith('http://'):  # Force HTTPS
+        url = request.url.replace('http://', 'https://', 1)
+        return redirect(url, code=301)
+    if not request.host.startswith('www.'):  # Force www
+        url = request.url.replace(request.host, 'www.' + request.host, 1)
+        return redirect(url, code=301)
 
 @app.route('/')
 def home():
